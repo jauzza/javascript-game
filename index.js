baa6ad78d4bd8da65cd41e2ae2ba3b2e73396c84
jauzza <jaydendylanodijkstra@gmail.com>
2023-06-20 17:13:35 +0200

    3de push player creation bijna af

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
 canvas.width = innerWidth
-canvas.width = innerHeight
+canvas.height = innerHeight
 
-class player {
-    constructor(){
+class Player {
+    constructor() {
         this.position = {
             x: 200,
             y: 200
@@ -15,17 +15,29 @@ class player {
             x: 0,
             y: 0
         }
-        //this.image =
-        this.width = 100
-        this.height = 100
+
+        const image = new Image()
+        image.src = './assets/img/ship.png'
+
+      this.image = image
+      this.width = 100
+      this.height = 100
     }
 
     draw() {
-        c.fillStyle = 'red'
-        c.fillRect(this.position.x, this.position.y, this.width,
-            this.height)
+        // c.fillStyle = 'red'
+        // c.fillRect(this.position.x, this.position.y, this.width, this.height)
+        c.drawImage(this.image, this.position.x, this.position.y)
     }
 }
 
-const player = new player()
+const player = new Player()
 player.draw()
+
+function animate() {
+    requestAnimationFrame(animate)
+    player.draw()
+    
+}
+
+animate()
